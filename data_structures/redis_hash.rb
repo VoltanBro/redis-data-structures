ccf1a8a9188a9d3833b288cd718c8d296e5f20d1
Vitalii Molchanov <etc.sharam@gmail.com>
2023-01-26 22:58:35 +0200

    add hash examples

---
+++ data_structures/redis_hash.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative '../redis_init'
+require 'securerandom'
+
+class RedisHash < RedisInit
+  def hmset(key, *values)
+    client.hmset(key, *values)
+  end
+
+  def hget(key, value)
+    client.hget(key, value)
+  end
+
+  def hgetall(key)
+    client.hgetall(key)
+  end
+
+  def hkeys(key)
+    client.hkeys(key)
+  end
+
+  def hvals(key)
+    client.hvals(key)
+  end
+
+  def hexists(key, value)
+    client.hexists(key, value)
+  end
+
+  def hdel(key, *values)
+    client.hdel(key, *values)
+  end
+end
+
+redis_hash = RedisHash.new
+redis_hash.hmset(:books, :jack_london, 'Whit Fang', :stephen_king, 'It')
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts "My favorite book of Stephen King is a #{redis_hash.hget(:books, :stephen_king)}"
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts "Our library has: #{redis_hash.hgetall(:books)}"
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts "In our library you is present next authors: #{redis_hash.hkeys(:books)}"
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts "In our library you is present next books: #{redis_hash.hvals(:books)}"
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts 'Let me check availability of this author'
+puts redis_hash.hexists(:books, :stephen_king) ? 'Yeah! We have it' : 'Sorry, this book is not available'
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+redis_hash.hdel(:books, :jack_london)
+puts 'Let me check availability of this author'
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts redis_hash.hexists(:books, :jack_london) ? 'Yeah! We have it' : 'Sorry, this book is not available'
--- data_structures/redis_string.rb
+++ data_structures/redis_string.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
-
-require 'redis'
 require 'securerandom'
+require_relative '../redis_init'
 
-class RedisString
-  def initialize
-    @client = Redis.new
-  end
-
+class RedisString < RedisInit
   def set(key, value)
     client.set(key, value)
   end
@@ -39,10 +34,6 @@ class RedisString
   def setex(key, seconds, value)
     client.setex(key, seconds, value)
   end
-
-  private
-
-  attr_reader :client
 end
 
 redis_string = RedisString.new
@@ -50,21 +41,31 @@ redis_string.set(:name, 'FooName')
 redis_string.set(:email, 'test@bar.com')
 redis_string.set(:year, 2000)
 
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts
 puts "Welcome #{redis_string.get(:name)}: your email is - #{redis_string.get(:email)}"
-
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts
 puts "Your previous email #{redis_string.getset(:email, 'second_test@bar.com')} was updated to second_test@bar.com"
-
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts
 puts "Your email has #{redis_string.strlen(:email)} symbols"
-
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts
 puts "The next year after #{redis_string.get(:year)} is a #{redis_string.incr(:year)}"
-
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts
 puts "Year was incremented by 20: #{redis_string.incrby(:year, 20)}"
-
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts
 redis_string.append(:email, '_deleted')
 puts "Suffix deleted was added to your email: #{redis_string.get(:email)}"
-
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts
 puts 'Token will be expired in 1 second'
 redis_string.setex(:token, 1, SecureRandom.hex(10))
 puts "Current token is: #{redis_string.get(:token)}"
 sleep 2
 puts 'Token was expired' unless redis_string.get(:token)
+puts '-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'
+puts
